55084339f27078c65d82e289192299daaedd9e52
unknown <temirbek.balabek@mail.ru>
2018-01-10 10:18:09 +0600

    lab1

---
+++ pp2 lab1/pp2 lab1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();//сандарды оқу
+            args = line.Split(' ');//әрбір санды бөліп жазу
+            foreach (string s in args)
+            {
+                int b = int.Parse(s);//стриңнан интқа ауыстыру
+                int k = 0;
+                for (int i = 2; i < b; i++)
+                {
+                    if (b % i == 0)
+                        k++;
+                }
+                if (k == 0 && b != 1)
+                    Console.WriteLine(b);//экранға шығару
+
+            }
+            Console.ReadKey();//жабылып қалмау үшін қолданылатын пауза
+        }
+    }
+}
