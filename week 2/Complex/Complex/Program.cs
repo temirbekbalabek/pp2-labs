1498b3a46d9c017d10c95126281145d4368f8013
unknown <temirbek.balabek@mail.ru>
2018-01-17 22:18:26 +0600

    week 2 task 4

---
+++ week 2/Complex/Complex/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string[] b = a.Split(' ');
+            List<int> c = new List<int>();
+            foreach(string s in b)
+            {
+                string[] d = s.Split('/');
+                c.Add(int.Parse(d[0]));
+                c.Add(int.Parse(d[1]));
+            }
+            complex e = new complex(c[0], c[1]);
+            complex f = new complex(c[2], c[3]);
+            complex g1 = e + f;
+            complex g2 = e - f;
+            complex g3 = e * f;
+            complex g4 = e / f;
+            
+            Console.WriteLine("the sum of two elements is = "+g1);
+            Console.WriteLine("the subtract of two elements is = "+g2);
+            Console.WriteLine("the product of two elements is = "+g3);
+            Console.WriteLine("the division of two elements is = "+g4);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ week 2/Complex/Complex/complex.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex
+{
+    class complex
+    {
+        int a, b;
+        public complex(int _a, int _b)
+        {
+            a = _a;
+            b = _b;
+        }
+        public static int lcmm(int e, int f)
+        {
+            int gcd=-1, lcm;
+            for (int i = Math.Min(e, f); i >= 1; i--)
+            {
+                if (Math.Max(e, f) % i == 0 && Math.Min(e, f) % i == 0)
+                {
+                    gcd = i;
+                    break;
+                }
+            }
+            lcm = (e * f) / (gcd);
+            return lcm;
+        }
+        public static complex operator +(complex x, complex y)
+        {
+            int lcm = lcmm(x.b, y.b);
+            int alymy = (lcm * x.a) / x.b + (lcm * y.a) / y.b;
+            int bolymy = lcm;
+            int gcd = (alymy * bolymy) / lcmm(alymy, bolymy);
+            complex g = new complex(alymy / gcd, bolymy / gcd);
+            return g;
+        }
+        public static complex operator *(complex x, complex y)
+        {
+            int alymy = x.a * y.a;
+            int bolymy = x.b * y.b;
+            int gcd = (alymy * bolymy) / lcmm(alymy, bolymy);
+            complex g = new complex(alymy/gcd, bolymy/gcd);
+            return g;
+        }
+        public static complex operator /(complex x, complex y)
+        {
+            int alymy = x.a * y.b;
+            int bolymy = x.b * y.a;
+            int gcd = (alymy * bolymy) / lcmm(alymy, bolymy);
+            complex g = new complex(alymy / gcd, bolymy / gcd);
+            return g;
+        }
+        public static complex operator -(complex x, complex y)
+        {
+            int lcm = lcmm(x.b, y.b);
+            int alymy = (lcm * x.a) / x.b - (lcm * y.a) / y.b;
+            int bolymy = lcm;
+            int gcd = (alymy * bolymy) / lcmm(alymy, bolymy);
+            complex g = new complex(alymy / gcd, bolymy / gcd);
+            return g;
+        }
+
+        public override string ToString()
+        {
+            return a + "/" + b;
+        }
+
+    }
+}
